7fefc495171d7c9afff04f19473e18cef0cdf302
Mayur Jadhav <mayursweet52@gmail.com>
2026-08-25 01:51:23 +0530

    feat: add root healthChecker alias supporting require('./healthChecker')

---
+++ healthChecker.js
@@ -0,0 +1,5 @@
+/**
+ * Rankly.ai - Production-Grade Self-Healing Background Engine
+ * Root alias exporting modular healthChecker service
+ */
+module.exports = require('./src/services/healthChecker');
